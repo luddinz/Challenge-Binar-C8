5a6f4e950bc67ae5d2f124b1fe0ad6e406980d27
luddinz <muhammadfarizluddinz@gmail.com>
2022-07-07 01:37:34 +0700

    Add little coding

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const apiRouter = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJSON = require("./docs/API.json");
 const PORT = process.env.PORT || 4000;
 
 // middlewares
@@ -10,6 +12,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(errorHandler);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerJSON));
 
 /**
  * @Routes /api
