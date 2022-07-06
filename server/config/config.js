56266427bd4a508272d2edc52403056d00bdb389
luddinz <muhammadfarizluddinz@gmail.com>
2022-07-07 01:18:33 +0700

    Sequelize DB Create and Migrate

--- server/config/config.js
+++ server/config/config.js
@@ -3,23 +3,23 @@ require("dotenv").config();
 module.exports = {
   development: {
     username: process.env.DB_USERNAME_DEV || "postgres",
-    password: process.env.DB_PASSWORD_DEV || "postgres",
+    password: process.env.DB_PASSWORD_DEV || "P@ssw0rd21",
     database: process.env.DB_DEV || "ch8-challenge_development",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "postgres",
   },
   test: {
     username: process.env.DB_USERNAME_TEST || "postgres",
-    password: process.env.DB_PASSWORD_TEST || "postgres",
+    password: process.env.DB_PASSWORD_TEST || "P@ssw0rd21",
     database: process.env.DB_TEST || "ch8-challenge_test",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "postgres",
   },
   production: {
     username: process.env.DB_USERNAME_PROD || "postgres",
-    password: process.env.DB_PASSWORD_PROD || "postgres",
+    password: process.env.DB_PASSWORD_PROD || "P@ssw0rd21",
     database: process.env.DB_PROD || "ch8-challenge_production",
-    host: "127.0.0.1",
+    host: "localhost",
     dialect: "postgres",
   },
 };
